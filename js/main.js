8ec9b286c9c306d1e5c2833c338baed43c29563f
Emmy <60683173+emmy-html@users.noreply.github.com>
2020-11-21 12:18:33 -0600

    Added CSS styles to remainder of the page

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@ function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
   }
+  function myFunction2() {
+    var popup = document.getElementById("myPopup2");
+    popup.classList.toggle("show");
+  }
+  function myFunction3() {
+    var popup = document.getElementById("myPopup3");
+    popup.classList.toggle("show");
+  }
+  function myFunction4() {
+    var popup = document.getElementById("myPopup4");
+    popup.classList.toggle("show");
+  }
